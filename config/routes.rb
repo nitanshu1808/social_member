23628c55adddc35525a27dcafd28401e036734dd
Nitanshu Rehani <nitanshu.nik@gmail.com>
2019-03-14 23:35:14 +0000

    - devise implementation for the admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admin, :controllers => {:registrations => "admin/registrations", :sessions => "admin/sessions"}
+
+  root to: "home#index"
+
+  authenticated :admin do
+    resources :members
+  end
 end
