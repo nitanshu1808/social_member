5d3e31758ef8743a586ba615b8feef96bc57986f
Nitanshu Rehani <nitanshu.nik@gmail.com>
2019-03-16 23:32:44 +0000

    - test spec for admin sign out

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
-RSpec.feature "Sign In Admin" do
+RSpec.feature "Sign In & Sign Out Admin" do
   let(:admin)           { create(:admin) }
 
   it "authenticate admin" do
     visit "/admin"
     sign_in( admin )
+    expect(page).to have_content(I18n.t("app.welcome", val: admin.name ))
+  end
+
+  it "destroys admin sesssions" do
+    visit "/admin"
+    sign_in( admin )
+    sign_out
   end
 end
--- spec/support/spec_test_helper.rb
+++ spec/support/spec_test_helper.rb
@@ -4,5 +4,11 @@ module SpecTestHelper
     fill_in I18n.t("model.admin.password"),      :with => user.password
     click_button I18n.t("app.sign_in")
     expect(page).to have_content(I18n.t("devise.sessions.signed_in"))
+    expect(page).to have_link(I18n.t("app.log_out"), href: destroy_admin_session_path)
+  end
+
+  def sign_out
+    click_link I18n.t("app.log_out")
+    expect(page).to have_content(I18n.t("devise.sessions.signed_out"))    
   end
 end
