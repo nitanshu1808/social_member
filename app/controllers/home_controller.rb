8469924c32b10aadb42b460ea2b7afb40067917b
Nitanshu Rehani <nitanshu.nik@gmail.com>
2019-03-18 09:50:03 +0000

    - changes in the home page - added member/admin sign in button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,6 @@
 class HomeController < ApplicationController
-  before_action :verify_member, if: :current_member
 
   def index
-    @member = Member.new
   end
 
-  private
-  def verify_member
-    redirect_to members_path, notice: I18n.t("devise.failure.already_authenticated")
-  end
 end
--- app/controllers/members/sessions_controller.rb
+++ app/controllers/members/sessions_controller.rb
@@ -2,16 +2,8 @@
 
 class Members::SessionsController < Devise::SessionsController
 
-  def create
-    @member = Member.find_by(email:login_params["email"])
-    if @member && @member.valid_password?(login_params["password"])
-      flash[:notice] = I18n.t("devise.sessions.signed_in")
-      sign_in_and_redirect(@member)
-    else
-      @member = Member.new(login_params)
-      @member.errors.add(:base, I18n.t('app.wrong_credentials'))
-      render 'home/index'
-    end
+  def new
+    super
   end
 
   private
@@ -21,10 +13,6 @@ class Members::SessionsController < Devise::SessionsController
   end
 
   def after_sign_out_path_for( resource )
-    root_path
-  end
-
-  def login_params
-    params.require(:member).permit(:email, :password)
+    new_member_session_path
   end
 end
